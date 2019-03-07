ed4483c57ec3764aefb4d7b50e8fee764bd38363
rra.roro@gmail.com <rra.roro@gmail.com>
2019-03-07 06:01:07 +0300

    Add factorial.h

---
+++ include/factorial.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <boost/multiprecision/gmp.hpp>
+#include <boost/math/special_functions/factorials.hpp>
+
+int my_factorial(int i)
+{
+      int ret = 1;
+      for (int j = 0; j < i; j++)
+      {
+            ret *= j;
+      }
+
+      return ret;
+}
+
+template <typename T>
+auto factorial(int i) -> T
+{
+      if constexpr (std::is_same<int, T>::value)
+            return my_factorial(i);
+      else
+            return static_cast<T>(boost::math::factorial<boost::multiprecision::mpf_float_100>(i));
+}
