23ea67de4daf84ba1dc9ffd8f1d3e3258f9c50c4
rra.roro@gmail.com <rra.roro@gmail.com>
2019-03-07 03:46:59 +0300

    Test 5 for ltrace

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ using namespace boost::math;
 int main(int argc, char* argv[])
 {
       int max_item = 10;
-/*
+
       ParserCommandLine PCL;
       PCL.AddFormatOfArg("?", no_argument, '?');
       PCL.AddFormatOfArg("help", no_argument, '?');
@@ -65,8 +65,8 @@ int main(int argc, char* argv[])
             const char* const ptrStr = PCL.Option['m'].ParamOption[0].data();
             from_chars(ptrStr, ptrStr + SizeParam, max_item);
       }
-*/
-      /*map<int, mpz_int> map_default_allocator;
+
+      map<int, mpz_int> map_default_allocator;
       for (int i = 0; i < max_item; i++)
       {
             map_default_allocator[i] = static_cast<mpz_int>(factorial<mpf_float_100>(i));
@@ -81,7 +81,7 @@ int main(int argc, char* argv[])
       for (auto& item : map_my_allocator)
       {
             cout << item.first << " " << item.second << "\n";
-      }*/
+      }
 
       custom_forward_list<int> list_default_allocator;
       for (int i = 0; i < max_item; ++i)
