81f50c10cd73eb6f1c3685155eba1b537ac1683d
rra.roro@gmail.com <rra.roro@gmail.com>
2019-03-07 05:52:41 +0300

    Test 10 for ltrace

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include "forward_list.h"
 
 using namespace std;
+using namespace boost::multiprecision;
+using namespace boost::math;
 
 void help()
 {
@@ -19,7 +21,7 @@ void help()
        Options:  
        -version    -get version of program
        -?          -about program (this info)
-       -max_item   -amount items in containers
+       -max_item   -amount items in containers. default 10
 )" << endl;
 }
 
@@ -39,8 +41,56 @@ int my_factorial(int i)
       return ret;
 }
 
-using namespace boost::multiprecision;
-using namespace boost::math;
+template <typename T>
+auto factorial(int i) -> T
+{
+      if constexpr (std::is_same<int, T>::value)
+            return my_factorial(i);
+      else
+            return static_cast<T>(factorial<mpf_float_100>(i));
+}
+
+template <typename T>
+void task_map(int max_item)
+{
+      map<int, T> map_default_allocator;
+      for (int i = 0; i < max_item; i++)
+      {
+            map_default_allocator[i] = factorial<T>(i);
+      }
+
+      map<int, T, less<T>, custom_allocator<pair<const int, T>, 10>> map_my_allocator;
+      for (int i = 0; i < max_item; i++)
+      {
+            map_my_allocator[i] = factorial<T>(i);
+      }
+
+      for (auto& item : map_my_allocator)
+      {
+            cout << item.first << " " << item.second << "\n";
+      }
+}
+
+void task_custom_forward_list(int max_item)
+{
+      custom_forward_list<int> list_default_allocator;
+      for (int i = 0; i < max_item; ++i)
+      {
+            list_default_allocator.push_front(i);
+      }
+
+      custom_forward_list<int, custom_allocator<int, 10>> list_my_allocator;
+      for (int i = 0; i < max_item; ++i)
+      {
+            list_my_allocator.push_front(i);
+      }
+
+      for (auto& obj : list_my_allocator)
+      {
+            cout << obj << " ";
+      }
+}
+
 
 #ifndef _TEST
 
@@ -78,59 +128,14 @@ int main(int argc, char* argv[])
 
       if (PCL.Option['m'])
       {
-            map<int, mpz_int> map_default_allocator;
-            for (int i = 0; i < max_item; i++)
-            {
-                  map_default_allocator[i] = static_cast<mpz_int>(factorial<mpf_float_100>(i));
-            }
-
-            map<int, mpz_int, less<mpz_int>, custom_allocator<pair<const int, mpz_int>, 10>> map_my_allocator;
-            for (int i = 0; i < max_item; i++)
-            {
-                  map_my_allocator[i] = static_cast<mpz_int>(factorial<mpf_float_100>(i));
-            }
-
-            for (auto& item : map_my_allocator)
-            {
-                  cout << item.first << " " << item.second << "\n";
-            }
+            task_map<mpz_int>(max_item);
       }
       else
       {
-            map<int, int> map_default_allocator;
-            for (int i = 0; i < max_item; i++)
-            {
-                  map_default_allocator[i] = my_factorial(i);
-            }
-
-            map<int, int, less<int>, custom_allocator<pair<const int, int>, 10>> map_my_allocator;
-            for (int i = 0; i < max_item; i++)
-            {
-                  map_my_allocator[i] = my_factorial(i);
-            }
-
-            for (auto& item : map_my_allocator)
-            {
-                  cout << item.first << " " << item.second << "\n";
-            }
+            task_map<int>(max_item);
       }
 
-      custom_forward_list<int> list_default_allocator;
-      for (int i = 0; i < max_item; ++i)
-      {
-            list_default_allocator.push_front(i);
-      }
-
-      custom_forward_list<int, custom_allocator<int, 10>> list_my_allocator;
-      for (int i = 0; i < max_item; ++i)
-      {
-            list_my_allocator.push_front(i);
-      }
-
-      for (auto& obj : list_my_allocator)
-      {
-            cout << obj << " ";
-      }
+      task_custom_forward_list(max_item);
 
       return 0;
 }
